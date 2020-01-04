91eeb9604738dee4a349ccbabe87d9ab5ed363d4
eph <eph@iMacPro.local>
2020-01-04 23:25:00 +0800

    更新 package-selected-packages 变量

--- emacs/init.el
+++ emacs/init.el
@@ -140,7 +140,15 @@ There are two things you can do about this warning:
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(spacemacs-theme which-key undo-tree)))
+ '(package-selected-packages
+    '(
+      evil
+      undo-tree
+      which-key
+      spacemacs-theme
+      neotree
+      centaur-tabs
+      )))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
