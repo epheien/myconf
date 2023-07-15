6faa621acc0d3848c3c96549bc97f30775f1f4a4
eph <eph@MacPro.local>
2023-07-15 17:18:31 +0800

    新增 nvim-tree (替代NERDTree) 和 indent_blankline 插件

--- vim/init.lua
+++ vim/init.lua
@@ -1,3 +1,24 @@
 -- 读取 vimrc
 local vimrc = vim.fn.stdpath("config") .. "/vimrc"
 vim.cmd.source(vimrc)
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    width = 31,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+require("indent_blankline").setup({
+})
