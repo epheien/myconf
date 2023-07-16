c3f18c44db20e36d0630cfcfd6381885ade241f4
eph <eph@MacPro.local>
2023-07-17 00:55:06 +0800

    修正 nvim-tree 乱更改窗口尺寸的问题

--- vim/nvim-init.lua
+++ vim/nvim-init.lua
@@ -129,6 +129,7 @@ require("nvim-tree").setup({
   },
   actions = {
     open_file = {
+      resize_window = false,
       window_picker = {
         enable = true,
         picker = vim.fn['myrc#GetWindowIdForNvimTreeToOpen'],
