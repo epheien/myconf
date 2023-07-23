31c28dfb35e1fdaa4fc7a50a4006c4025e57e6a4
eph <eph@MacPro.local>
2023-07-23 18:25:04 +0800

    update nvim-tree options

--- vim/lua/config/nvim-tree.lua
+++ vim/lua/config/nvim-tree.lua
@@ -96,6 +96,7 @@ local function my_on_attach(bufnr)
   api.config.mappings.default_on_attach(bufnr)
 
   -- your removals and mappings go here
+  vim.keymap.del('n', '.', opts('Run Command'))
   local ok, mod = pcall(require, 'telescope')
   if ok then
     vim.keymap.set("n", ".", tree_actions_menu, opts("nvim tree menu"))
