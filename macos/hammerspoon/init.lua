8a48e44438cff8053f559a4fd7df79fb5a8ed4e0
eph <eph@MacBook-Pro.local>
2024-05-23 16:46:37 +0800

    minor update

--- macos/hammerspoon/init.lua
+++ macos/hammerspoon/init.lua
@@ -103,14 +103,13 @@ function toggleInputMethod()
     hs.eventtap.event.newSystemKeyEvent('CAPS_LOCK', false):post()
   else
     -- NOTE: 使用 ctrl-space 快捷键的话, 可能会导致按下左 cmd 的时候弹出表情输入框
-    local ctrlDown = hs.eventtap.event.newKeyEvent("cmd", true):post()
-    --hs.timer.usleep(1000)
-    local spaceDown = hs.eventtap.event.newKeyEvent("f17", true):post()
-    --hs.timer.usleep(1000)
-    local spaceUp = hs.eventtap.event.newKeyEvent("f17", false):post()
-    --hs.timer.usleep(1000)
-    local spaceUp = hs.eventtap.event.newKeyEvent("cmd", false):post()
-    --hs.timer.usleep(1000)
+    hs.eventtap.event.newKeyEvent("cmd", true):post()
+    hs.timer.usleep(10000)
+    hs.eventtap.event.newKeyEvent("f17", true):post()
+    hs.timer.usleep(10000)
+    hs.eventtap.event.newKeyEvent("f17", false):post()
+    hs.timer.usleep(10000)
+    hs.eventtap.event.newKeyEvent("cmd", false):post()
   end
   --hs.alert.show("Toggle Input Method")
 end
