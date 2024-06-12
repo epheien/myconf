7cf783b729dc61dd8e2ec848448eb9c80323b1e7
eph <eph@MacBook-Pro.local>
2024-06-12 13:55:20 +0800

    优化 incline 的配置方式

--- vim/init-nvim.lua
+++ vim/init-nvim.lua
@@ -179,16 +179,6 @@ local mode_table = {
   ['!']      = 'SHELL',
   ['t']      = 'TERMINAL',
 }
--- 高亮组初始化
-local function setup_statusline_hlgroup()
-  --vim.cmd([[hi InclineNormalMode guifg=#282828 guibg=#E0E000 ctermfg=235 ctermbg=184]])
-  vim.api.nvim_set_hl(0, 'InclineNormalMode', {fg='#282828', bg='#E0E000', ctermfg=235, ctermbg=184})
-  vim.cmd([[hi! IncelineInsertMode ctermfg=235 ctermbg=119 guibg=#95e454]])
-  vim.cmd([[hi! IncelineTerminalMode ctermfg=235 ctermbg=119 guibg=#95e454]])
-  vim.cmd([[hi! IncelineVisualMode ctermfg=235 ctermbg=216 guibg=#f2c68a]])
-  vim.cmd([[hi! IncelineSelectMode ctermfg=235 ctermbg=216 guibg=#f2c68a]])
-  vim.cmd([[hi! IncelineReplaceMode ctermfg=236 ctermbg=203 guifg=#353535 guibg=#e5786d]])
-end
 local function make_mode_display(m)
   local group = 'InclineNormalMode'
   local mode = mode_table[m]
@@ -207,116 +197,127 @@ local function make_mode_display(m)
   else
     -- 兜底用默认值
   end
-  -- 生成需要用到的高亮组
-  setup_statusline_hlgroup()
   return {
     string.format(' %s ', m),
     group = group,
   }
 end
-lazysetup('incline', {
-  debounce_threshold = {
-    falling = 50,
-    rising = 10
-  },
-  hide = {
-    cursorline = false,
-    focused_win = false,
-    only_win = false
-  },
-  highlight = {
-    groups = {
-      InclineNormal = InclineNormal,
-      InclineNormalNC = InclineNormalNC,
-    }
-  },
-  ignore = {
-    buftypes = {},
-    filetypes = {},
-    floating_wins = true,
-    unlisted_buffers = false,
-    wintypes = {}
-  },
-  window = {
-    margin = {
-      horizontal = 0,
-      vertical = 0
+function setup_incline()
+  -- 生成需要用到的高亮组
+  --vim.cmd([[hi InclineNormalMode guifg=#282828 guibg=#E0E000 ctermfg=235 ctermbg=184]])
+  vim.api.nvim_set_hl(0, 'InclineNormalMode', {fg='#282828', bg='#E0E000', ctermfg=235, ctermbg=184})
+  vim.cmd([[hi! IncelineInsertMode ctermfg=235 ctermbg=119 guibg=#95e454]])
+  vim.cmd([[hi! IncelineTerminalMode ctermfg=235 ctermbg=119 guibg=#95e454]])
+  vim.cmd([[hi! IncelineVisualMode ctermfg=235 ctermbg=216 guibg=#f2c68a]])
+  vim.cmd([[hi! IncelineSelectMode ctermfg=235 ctermbg=216 guibg=#f2c68a]])
+  vim.cmd([[hi! IncelineReplaceMode ctermfg=236 ctermbg=203 guifg=#353535 guibg=#e5786d]])
+  -- set noshowmode
+  vim.o.showmode = false
+  lazysetup('incline', {
+    debounce_threshold = {
+      falling = 50,
+      rising = 10
     },
-    options = {
-      signcolumn = "no",
-      wrap = false
+    hide = {
+      cursorline = false,
+      focused_win = false,
+      only_win = false
     },
-    overlap = {
-      borders = true,
-      statusline = true,
-      tabline = false,
-      winbar = false
-    },
-    padding = 0,
-    padding_char = " ",
-    placement = {
-      horizontal = "left",
-      vertical = "bottom"
-    },
-    width = "fit",
-    winhighlight = {
-      active = {
-        EndOfBuffer = "None",
-        Normal = "InclineNormal",
-        Search = "None"
-      },
-      inactive = {
-        EndOfBuffer = "None",
-        Normal = "InclineNormalNC",
-        Search = "None"
+    highlight = {
+      groups = {
+        InclineNormal = InclineNormal,
+        InclineNormalNC = InclineNormalNC,
       }
     },
-    zindex = 50
-  },
-  render = function(props)
-    local filename = vim.api.nvim_eval_statusline('%f', {winid = props.win})['str']
-    local mode = {}
-    local mod = vim.api.nvim_eval_statusline('%m%r', {winid = props.win})['str']
-    local active = (vim.api.nvim_tabpage_get_win(0) == props.win)
-    local left_icon = ' '
-    local trail_icon = ' '
-    local mode_padding = ''
-    -- 全局的背景色
-    local guibg = '#282828'
-    local ctermbg = 235
-    if vim.fn.OnlyASCII() == 0 then
-      --    
-      if active then
-        --left_icon = {'', guibg = guibg, guifg = InclineNormal.guibg, ctermbg = ctermbg, ctermfg = InclineNormal.ctermbg}
-        trail_icon = {'', guibg = guibg, guifg = InclineNormal.guibg, ctermbg = ctermbg, ctermfg = InclineNormal.ctermbg}
-        mode = make_mode_display(vim.fn.mode())
-        if #mode ~= 0 then mode_padding = ' '; left_icon = '' end
-      else
-        --left_icon = {'', guibg = guibg, guifg = InclineNormalNC.guibg, ctermbg = ctermbg, ctermfg = InclineNormalNC.ctermbg}
-        trail_icon = {'', guibg = guibg, guifg = InclineNormalNC.guibg, ctermbg = ctermbg, ctermfg = InclineNormalNC.ctermbg}
+    ignore = {
+      buftypes = {},
+      filetypes = {},
+      floating_wins = true,
+      unlisted_buffers = false,
+      wintypes = {}
+    },
+    window = {
+      margin = {
+        horizontal = 0,
+        vertical = 0
+      },
+      options = {
+        signcolumn = "no",
+        wrap = false
+      },
+      overlap = {
+        borders = true,
+        statusline = true,
+        tabline = false,
+        winbar = false
+      },
+      padding = 0,
+      padding_char = " ",
+      placement = {
+        horizontal = "left",
+        vertical = "bottom"
+      },
+      width = "fit",
+      winhighlight = {
+        active = {
+          EndOfBuffer = "None",
+          Normal = "InclineNormal",
+          Search = "None"
+        },
+        inactive = {
+          EndOfBuffer = "None",
+          Normal = "InclineNormalNC",
+          Search = "None"
+        }
+      },
+      zindex = 50
+    },
+    render = function(props)
+      local filename = vim.api.nvim_eval_statusline('%f', {winid = props.win})['str']
+      local mode = {}
+      local mod = vim.api.nvim_eval_statusline('%m%r', {winid = props.win})['str']
+      local active = (vim.api.nvim_tabpage_get_win(0) == props.win)
+      local left_icon = ' '
+      local trail_icon = ' '
+      local mode_padding = ''
+      -- 全局的背景色
+      local guibg = '#282828'
+      local ctermbg = 235
+      if vim.fn.OnlyASCII() == 0 then
+        --    
+        if active then
+          --left_icon = {'', guibg = guibg, guifg = InclineNormal.guibg, ctermbg = ctermbg, ctermfg = InclineNormal.ctermbg}
+          trail_icon = {'', guibg = guibg, guifg = InclineNormal.guibg, ctermbg = ctermbg, ctermfg = InclineNormal.ctermbg}
+          mode = make_mode_display(vim.fn.mode())
+          if #mode ~= 0 then mode_padding = ' '; left_icon = '' end
+        else
+          --left_icon = {'', guibg = guibg, guifg = InclineNormalNC.guibg, ctermbg = ctermbg, ctermfg = InclineNormalNC.ctermbg}
+          trail_icon = {'', guibg = guibg, guifg = InclineNormalNC.guibg, ctermbg = ctermbg, ctermfg = InclineNormalNC.ctermbg}
+        end
       end
-    end
-    if mod ~= '' then
-      mod = ' ' .. mod
-    end
-    local result = {
-      left_icon,
-      mode,
-      mode_padding,
-      filename,
-      mod,
-    }
+      if mod ~= '' then
+        mod = ' ' .. mod
+      end
+      local result = {
+        left_icon,
+        mode,
+        mode_padding,
+        filename,
+        mod,
+      }
 
-    -- 简单地添加 ruler 信息
-    local ruler = vim.api.nvim_eval_statusline(' %l/%L,%v ', {winid = props.win})['str']
-    ruler = ' │' .. ruler
-    table.insert(result, ruler)
+      -- 简单地添加 ruler 信息
+      local ruler = vim.api.nvim_eval_statusline(' %l/%L,%v ', {winid = props.win})['str']
+      ruler = ' │' .. ruler
+      table.insert(result, ruler)
 
-    table.insert(result, trail_icon)
-    return result
-  end,
-})
+      table.insert(result, trail_icon)
+      return result
+    end,
+  })
+end
 -- }}}
+setup_incline()
 
 function CscopeFind(op, symbol)
   local cscope = require('cscope')
