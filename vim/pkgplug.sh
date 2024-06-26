a67ed67ecddf706882921c2cb03d6414de983cbd
eph <eph@MacBook-Pro.local>
2024-06-26 17:24:49 +0800

    整理 coc.nvim 配置, 并将 coc 插件安装位置移动到 vimrc 目录

--- vim/pkgplug.sh
+++ vim/pkgplug.sh
@@ -2,5 +2,5 @@
 # 打包所有插件以用于一键在新环境安装全套插件
 set -e
 output=vim-plugins.tar.gz
-tar --no-mac-metadata --no-xattrs -czf "$output" pckr/ plugged/
+tar --no-mac-metadata --no-xattrs -czf "$output" pckr/ plugged/ coc/
 echo "Done: $output"
