7cd8d0ffdbc411e1da381b057c45091db94d35b7
eph <eph@MacBook-Pro.local>
2024-06-28 18:30:39 +0800

    lspconfig 的懒加载准备

--- vim/lua/config/nvim-lspconfig.lua
+++ vim/lua/config/nvim-lspconfig.lua
@@ -1,46 +1,51 @@
 local lspconfig = require('lspconfig')
-lspconfig.pyright.setup({})
-lspconfig.lua_ls.setup({
-  on_init = function(client)
-    local path = client.workspace_folders[1].name
-    if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
-      return
-    end
+-- TODO: setup() 的懒加载
+local lsp_setup = function(event)
+  lspconfig.pyright.setup({})
+  lspconfig.lua_ls.setup({
+    on_init = function(client)
+      local path = client.workspace_folders[1].name
+      if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+        return
+      end
 
-    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-      runtime = {
-        -- Tell the language server which version of Lua you're using
-        -- (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT'
-      },
-      -- Make the server aware of Neovim runtime files
-      workspace = {
-        checkThirdParty = false,
-        library = {
-          vim.env.VIMRUNTIME
-          -- Depending on the usage, you might want to add additional paths here.
-          -- "${3rd}/luv/library"
-          -- "${3rd}/busted/library",
+      client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+        runtime = {
+          -- Tell the language server which version of Lua you're using
+          -- (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT'
+        },
+        -- Make the server aware of Neovim runtime files
+        workspace = {
+          checkThirdParty = false,
+          library = {
+            vim.env.VIMRUNTIME
+            -- Depending on the usage, you might want to add additional paths here.
+            -- "${3rd}/luv/library"
+            -- "${3rd}/busted/library",
+          }
+          -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+          -- library = vim.api.nvim_get_runtime_file("", true)
         }
-        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-        -- library = vim.api.nvim_get_runtime_file("", true)
-      }
-    })
-  end,
-  settings = {
-    Lua = {}
-  }
-})
-lspconfig.clangd.setup({
-  cmd = {
-    'clangd',
-    '--header-insertion=never', -- NOTE: 添加这个选项后, '•' 前缀变成了 ' ', 需要自己过滤掉
-    --'--header-insertion-decorators',
-  },
-})
+      })
+    end,
+    settings = {
+      Lua = {}
+    }
+  })
+  lspconfig.clangd.setup({
+    cmd = {
+      'clangd',
+      '--header-insertion=never', -- NOTE: 添加这个选项后, '•' 前缀变成了 ' ', 需要自己过滤掉
+      --'--header-insertion-decorators',
+    },
+  })
+end
 vim.diagnostic.config({signs = false})
 vim.diagnostic.config({underline = false})
 -- Hide all semantic highlights
 for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
   vim.api.nvim_set_hl(0, group, {})
 end
+
+lsp_setup()
