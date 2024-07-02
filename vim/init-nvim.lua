7b54bfdcceff59a094c0dae8ba85cd4ac4d3bec2
eph <eph@MacBook-Pro.local>
2024-07-02 17:57:22 +0800

    移除无用代码

--- vim/init-nvim.lua
+++ vim/init-nvim.lua
@@ -32,51 +32,6 @@ local function setup_cscope_maps() --{{{
 end
 --}}}
 
-local function setup_telescope() -- {{{
-  local opts = {
-    -- Default configuration for telescope goes here:
-    -- config_key = value,
-    mappings = {
-      i = {
-        ["<Esc>"] = require('telescope.actions').close,
-        ["<C-j>"] = require('telescope.actions').move_selection_next,
-        ["<C-k>"] = require('telescope.actions').move_selection_previous,
-        ["<C-l>"] = false,
-        ["<C-u>"] = false,
-        ["<C-b>"] = require('telescope.actions').preview_scrolling_up,
-        ["<C-f>"] = require('telescope.actions').preview_scrolling_down,
-      }
-    },
-    sorting_strategy = "ascending",
-    layout_config = {
-      prompt_position = 'top',
-    },
-  }
-  lazysetup('telescope', function(mod)
-    mod.setup({
-      --defaults = require('telescope.themes').get_dropdown(opts),
-      defaults = opts,
-      pickers = {
-        tags = {
-          only_sort_tags = true,
-        },
-        current_buffer_tags = {
-          only_sort_tags = true,
-        },
-        find_files = {
-          no_ignore = true,
-          no_ignore_parent = true,
-        },
-      },
-    })
-  end)
-  vim.api.nvim_set_hl(0, 'TelescopeBorder', {link = 'WinSeparator', force = true})
-  vim.api.nvim_set_hl(0, 'TelescopeTitle', {link = 'Title', force = true})
-  -- telescope 的 undo 插件
-  require("telescope").load_extension("undo")
-end
---}}}
-
 local function setup_noice() -- {{{
   lazysetup('noice', {
     override = {
