3bb306d5918b8890c421cd38b171b48e6c2851f2
eph <eph@MacBook-Pro.local>
2024-07-06 15:11:21 +0800

    update config

--- vim/init-nvim.lua
+++ vim/init-nvim.lua
@@ -182,6 +182,7 @@ local function setup_pckr() -- {{{
       'nvim-lualine/lualine.nvim',
       config = function()
         vim.opt.laststatus = 2
+        vim.opt.showmode = false
         setup_lualine()
       end
     };
