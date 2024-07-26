573b4534d8dbb091cc2446c9fcdb84db736a2db6
eph <eph@MacPro.local>
2024-07-26 12:03:46 +0800

    修正部分情况下, 无法正确诊断 nvim lua 代码的问题

--- vim/lua/config/lsp/lua_ls.lua
+++ vim/lua/config/lsp/lua_ls.lua
@@ -1,9 +1,9 @@
 return {
   on_init = function(client)
-    local path = client.workspace_folders[1].name
-    if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
-      return
-    end
+    --local path = client.workspace_folders[1].name
+    --if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+    --  return
+    --end
 
     client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
       runtime = {
