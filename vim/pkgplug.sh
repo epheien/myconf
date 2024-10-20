47216bae779d29bc80d91a74446e9df5ff5fb854
eph <eph@ubuntu2204.local>
2024-10-20 16:12:54 +0800

    update pkgplug.sh

--- vim/pkgplug.sh
+++ vim/pkgplug.sh
@@ -2,5 +2,9 @@
 # 打包所有插件以用于一键在新环境安装全套插件
 set -e
 output=vim-plugins.tar.gz
-tar --no-mac-metadata --no-xattrs -czf "$output" pckr/ plugged/ coc/
+if [ -d /proc/ ]; then
+    tar --no-xattrs -czf "$output" pckr/ plugged/ coc/
+else
+    tar --no-mac-metadata --no-xattrs -czf "$output" pckr/ plugged/ coc/
+fi
 echo "Done: $output"
