e4a13a2d816e6d5397a6cd185c8971c2b4d1854f
eph <eph@MacBook-Pro.local>
2024-11-23 18:17:10 +0800

    pkgplug.sh: 添加 -m 参数

--- vim/pkgplug.sh
+++ vim/pkgplug.sh
@@ -2,9 +2,18 @@
 # 打包所有插件以用于一键在新环境安装全套插件
 set -e
 output=vim-plugins.tar.gz
+
 if [ -d /proc/ ]; then
-    tar --no-xattrs -czf "$output" pckr/ plugged/ coc/
+    cmd="tar --no-xattrs"
 else
-    tar --no-mac-metadata --no-xattrs -czf "$output" pckr/ plugged/ coc/
+    cmd="tar --no-mac-metadata --no-xattrs"
 fi
+
+# -m means minimize
+if [ "$1" == "-m" ]; then
+    cmd="$cmd --exclude 'coc/*' --exclude '*/coc.nvim/*' --exclude videm --exclude '*/vimcdoc/*'"
+fi
+
+eval $cmd -czf "$output" pckr/ plugged/ coc/
+
 echo "Done: $output"
