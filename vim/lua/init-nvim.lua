312dc5c7f5a32d58e573582f482edab1cc3793b4
eph <eph@MacBook-Pro.local>
2024-12-26 02:36:38 +0800

    添加 conform.nvim 插件

--- vim/lua/init-nvim.lua
+++ vim/lua/init-nvim.lua
@@ -599,6 +599,34 @@ local function setup_pckr() -- {{{
     requires = { 'nvim-lua/plenary.nvim' },
   })
 
+  table.insert(plugins, {
+    'stevearc/conform.nvim',
+    cond = cmd('Format'),
+    config = function()
+      local conform = require('conform')
+      conform.setup({
+        formatters_by_ft = {
+          lua = { lsp_format = 'prefer' },
+          cpp = { 'clang-format' },
+        },
+        format_on_save = function() end,
+        format_after_save = function() end,
+      })
+
+      vim.api.nvim_create_user_command("Format", function(args)
+        local range = nil
+        if args.count ~= -1 then
+          local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+          range = {
+            start = { args.line1, 0 },
+            ["end"] = { args.line2, end_line:len() },
+          }
+        end
+        conform.format({ async = true, lsp_format = "fallback", range = range })
+      end, { range = true })
+    end
+  })
+
   pckr.add(plugins)
 end
 -- }}}
