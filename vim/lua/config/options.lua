c6cf41b19b650b5170005f41b312eb4d5e228c37
eph <eph@MacBook-Pro.local>
2024-12-30 02:46:29 +0800

    把部分 vim 脚本转为 lua

--- vim/lua/config/options.lua
+++ vim/lua/config/options.lua
@@ -157,39 +157,36 @@ vim.g.NERDCustomDelimiters = {
   }
 }
 
-vim.cmd([[
-" ========== neosnippet ==========
-let g:neosnippet#snippets_directory = [expand('~/.vim/snippets')]
-" ========== coc.nvim ==========
-" CocInstall coc-neosnippet
-" CocInstall coc-json
-let g:coc_snippet_next = ''
-let g:coc_snippet_prev = ''
-let g:coc_data_home = stdpath('config') .. '/coc'
-" 补全后自动弹出函数参数提示
-" 一般按<CR>确认补全函数后, 会自动添加括号并让光标置于括号中
-autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
-]])
+-- neosnippet
+---@diagnostic disable-next-line
+vim.g['neosnippet#snippets_directory'] = vim.fs.joinpath(vim.fn.stdpath('config'), 'snippets')
+-- ========== coc.nvim ==========
+-- CocInstall coc-neosnippet
+-- CocInstall coc-json
+vim.g.coc_snippet_next = ''
+vim.g.coc_snippet_prev = ''
+---@diagnostic disable-next-line
+vim.g.coc_data_home = vim.fs.joinpath(vim.fn.stdpath('config'), 'coc')
+-- 补全后自动弹出函数参数提示
+-- 一般按<CR>确认补全函数后, 会自动添加括号并让光标置于括号中
+vim.cmd([[autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')]])
 
-vim.cmd([[
-" ========== clang-format ==========
-let g:clang_format#code_style = 'llvm'
-" NOTE: 不能指定 IndentWidth 和 UseTab, 因为插件自动设置了, 重复设置会出错!
-let g:clang_format#style_options = {
-    "\ 'IndentWidth': 4,
-    \ 'AlwaysBreakTemplateDeclarations': v:true,
-    \ 'BinPackArguments': v:false,
-    \ 'BinPackParameters': v:false,
-    \ 'ColumnLimit': 100,
-    \ 'IndentCaseLabels': v:false,
-    \ 'DerivePointerAlignment': v:false,
-    \ 'PointerAlignment': 'Left',
-    \ 'AccessModifierOffset': -4,
-    \ 'ReflowComments': v:false,
-    \ 'SortIncludes': v:true,
-    \ 'IncludeBlocks': 'Preserve',
-    \ 'AllowShortFunctionsOnASingleLine': 'Empty',
-    \ 'AllowShortIfStatementsOnASingleLine': v:false,
-    \ 'SpacesBeforeTrailingComments': 1,
-    \ }
-]])
+-- clang-format
+-- NOTE: 不能指定 IndentWidth 和 UseTab, 因为插件自动设置了, 重复设置会出错!
+vim.g['clang_format#code_style'] = 'llvm'
+vim.g['clang_format#style_options'] = {
+  AccessModifierOffset = -4,
+  AllowShortFunctionsOnASingleLine = "Empty",
+  AllowShortIfStatementsOnASingleLine = false,
+  AlwaysBreakTemplateDeclarations = true,
+  BinPackArguments = false,
+  BinPackParameters = false,
+  ColumnLimit = 100,
+  DerivePointerAlignment = false,
+  IncludeBlocks = "Preserve",
+  IndentCaseLabels = false,
+  PointerAlignment = "Left",
+  ReflowComments = false,
+  SortIncludes = true,
+  SpacesBeforeTrailingComments = 1
+}
