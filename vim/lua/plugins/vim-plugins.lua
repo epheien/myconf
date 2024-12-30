f1e2fc2bcbc6ae0d8eb2aa0a6eeefcf1f174484d
eph <eph@MacBook-Pro.local>
2024-12-30 19:28:20 +0800

    update vim plugins lazyload

--- vim/lua/plugins/vim-plugins.lua
+++ vim/lua/plugins/vim-plugins.lua
@@ -1,18 +1,24 @@
 local vim_plugins = {
-  { 'yianwillis/vimcdoc' },
-  { 'tpope/vim-surround' },
-  { 'dstein64/vim-startuptime',    cmd = 'StartupTime' },
-  { 'sunaku/vim-dasht',            cmd = 'Dasht' },
-  { 'yuratomo/w3m.vim',            cmd = 'W3m' },
-  { 'Yggdroot/LeaderF',            cmd = 'Leaderf' },
-  { 'mbbill/undotree',             cmd = 'UndotreeShow' },
-  { 'dhruvasagar/vim-table-mode',  cmd = 'TableModeToggle' },
-  { 'skywind3000/asyncrun.vim',    cmd = 'AsyncRun' },
-  { 'kassio/neoterm',              cmd = 'Tnew' },
-  { 'epheien/vim-clang-format',    cmd = 'ClangFormat' },
-  { 'epheien/videm',               cmd = 'VidemOpen' },
+  { 'tpope/vim-surround', event = 'CursorMoved' },
+  {
+    'yianwillis/vimcdoc',
+    event = 'CmdlineEnter',
+    config = function()
+      vim.o.helplang = ''
+    end,
+  },
+  { 'dstein64/vim-startuptime', cmd = 'StartupTime' },
+  { 'sunaku/vim-dasht', cmd = 'Dasht' },
+  { 'yuratomo/w3m.vim', cmd = 'W3m' },
+  { 'Yggdroot/LeaderF', cmd = 'Leaderf' },
+  { 'mbbill/undotree', cmd = 'UndotreeShow' },
+  { 'dhruvasagar/vim-table-mode', cmd = 'TableModeToggle' },
+  { 'skywind3000/asyncrun.vim', cmd = 'AsyncRun' },
+  { 'kassio/neoterm', cmd = 'Tnew' },
+  { 'epheien/vim-clang-format', cmd = 'ClangFormat' },
+  { 'epheien/videm', cmd = 'VidemOpen' },
   { 'iamcco/markdown-preview.vim', ft = 'markdown' },
-  { 'tweekmonster/helpful.vim',    cmd = 'HelpfulVersion', },
+  { 'tweekmonster/helpful.vim', cmd = 'HelpfulVersion' },
 }
 
 return vim_plugins
