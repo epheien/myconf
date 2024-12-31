c792824d7608c3d7895a08188a66c5ab18f62b77
eph <eph@MacBook-Pro.local>
2024-12-31 23:36:33 +0800

    设置 dashboard 的 header 高亮组

--- vim/lua/plugins/snacks.lua
+++ vim/lua/plugins/snacks.lua
@@ -1,30 +1,35 @@
+local opts = {
+  styles = {
+    dashboard = {
+      wo = {
+        --winhighlight = 'Normal:SnacksDashboardNormal,NormalFloat:SnacksDashboardNormal,Title:Directory',
+      },
+    },
+  },
+
+  dashboard = {
+    preset = {
+      -- stylua: ignore
+      keys = {
+        { icon = " ", key = "F", desc = "Find Manager", action = ":enew | NvimTreeOpen" },
+        { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+        { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+        { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+        { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+        { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+        { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+        { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+      },
+    },
+  },
+}
+
 return {
   'folke/snacks.nvim',
   priority = 1000,
   lazy = false,
-  opts = {
-    styles = {
-      dashboard = {
-        wo = {
-          --winhighlight = 'Normal:SnacksDashboardNormal,NormalFloat:SnacksDashboardNormal,Title:Directory',
-        },
-      },
-    },
-
-    dashboard = {
-      preset = {
-        -- stylua: ignore
-        keys = {
-          { icon = " ", key = "F", desc = "Find Manager", action = ":enew | NvimTreeOpen" },
-          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
-          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-        },
-      },
-    },
-  },
+  config = function()
+    require('snacks').setup(opts)
+    vim.api.nvim_set_hl(0, 'SnacksDashboardHeader', { link = 'Directory' })
+  end,
 }
