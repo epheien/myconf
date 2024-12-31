fda4fc8d716789e9818f881f6f94bfa34dbd9306
eph <eph@MacBook-Pro.local>
2024-12-31 21:33:28 +0800

    add snacks.nvim

---
+++ vim/lua/plugins/snacks.lua
@@ -0,0 +1,22 @@
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  opts = {
+    dashboard = {
+      preset = {
+        -- stylua: ignore
+        keys = {
+          { icon = " ", key = "F", desc = "File Manager", action = ":NvimTreeOpen" },
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+    },
+  },
+}
