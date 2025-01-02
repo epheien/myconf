22c369e5552fe447ad5a5fb442bb36261fce7625
eph <eph@MacBook-Pro.local>
2025-01-02 23:38:27 +0800

    设置 tokyonight 主题

--- vim/lua/plugins/tokyonight.lua
+++ vim/lua/plugins/tokyonight.lua
@@ -3,7 +3,13 @@ return {
   lazy = vim.g.my_colors_name ~= 'tokyonight',
   priority = 1000,
   config = function()
-    require('tokyonight').setup({})
+    require('tokyonight').setup({
+      style = 'moon', -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+      styles = {
+        comments = { italic = false },
+        keywords = { italic = false },
+      },
+    })
     require('tokyonight').load()
     vim.api.nvim_exec_autocmds('ColorScheme', { modeline = false })
   end,
