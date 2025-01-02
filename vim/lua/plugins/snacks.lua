6031ce100f8bc88655480fa8c7f33a3ef0e75b0f
eph <eph@MacBook-Pro.local>
2025-01-02 17:02:48 +0800

    update snacks.dashboard config

--- vim/lua/plugins/snacks.lua
+++ vim/lua/plugins/snacks.lua
@@ -25,25 +25,28 @@ local opts = {
     },
 
     sections = {
-      {
-        section = 'terminal',
-        --cmd = 'chafa vscode-neovim.png --format symbols --symbols vhalf --size 20x10 --stretch; sleep .1',
-        cmd = string.format(
-          'cat \'%s\'; sleep .1',
-          ---@diagnostic disable-next-line
-          vim.fs.joinpath(vim.fn.stdpath('config'), 'images', 'logo_cat.cat')
-        ),
-        height = 17,
-        padding = 1,
-        indent = 5,
-        ttl = math.huge, -- 缓存有效时间, 单位是秒
-      },
-      --{ section = 'header' },
+      { section = 'header' },
       { section = 'keys', gap = 1, padding = 1 },
       { section = 'startup' },
     },
   },
 }
+
+if false then
+  opts.dashboard.sections[1] = {
+    section = 'terminal',
+    --cmd = 'chafa vscode-neovim.png --format symbols --symbols vhalf --size 20x10 --stretch; sleep .1',
+    cmd = string.format(
+      'cat \'%s\'; sleep .1',
+      ---@diagnostic disable-next-line
+      vim.fs.joinpath(vim.fn.stdpath('config'), 'images', 'logo_cat.cat')
+    ),
+    height = 17,
+    padding = 1,
+    indent = 5,
+    ttl = math.huge, -- 缓存有效时间, 单位是秒
+  }
+end
 
 return {
   'folke/snacks.nvim',
