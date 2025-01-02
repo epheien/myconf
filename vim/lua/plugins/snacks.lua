038389ebec8b3907f43f83bec3d926a2725416d8
eph <eph@MacBook-Pro.local>
2025-01-02 14:10:39 +0800

    暂时不使用图片 log

--- vim/lua/plugins/snacks.lua
+++ vim/lua/plugins/snacks.lua
@@ -25,19 +25,19 @@ local opts = {
     },
 
     sections = {
-      {
-        section = 'terminal',
-        --cmd = 'chafa vscode-neovim.png --format symbols --symbols vhalf --size 20x10 --stretch; sleep .1',
-        cmd = string.format(
-          'cat \'%s\'; sleep .1',
-          ---@diagnostic disable-next-line
-          vim.fs.joinpath(vim.fn.stdpath('config'), 'images', 'vscode-neovim.cat')
-        ),
-        height = 10,
-        padding = 1,
-        indent = 20,
-      },
-      --{ section = 'header' },
+      --{
+      --  section = 'terminal',
+      --  --cmd = 'chafa vscode-neovim.png --format symbols --symbols vhalf --size 20x10 --stretch; sleep .1',
+      --  cmd = string.format(
+      --    'cat \'%s\'; sleep .1',
+      --    ---@diagnostic disable-next-line
+      --    vim.fs.joinpath(vim.fn.stdpath('config'), 'images', 'vscode-neovim.cat')
+      --  ),
+      --  height = 10,
+      --  padding = 1,
+      --  indent = 20,
+      --},
+      { section = 'header' },
       { section = 'keys', gap = 1, padding = 1 },
       { section = 'startup' },
     },
