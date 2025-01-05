29f52ea73f25db80b7ca8d0672f6afb87422a0ca
eph <eph@MacBook-Pro.local>
2025-01-06 00:31:00 +0800

    修正使用 nvim_buf_set_mark() 的问题

--- vim/after/ftplugin/lua.lua
+++ vim/after/ftplugin/lua.lua
@@ -1,9 +1,5 @@
 vim.keymap.set({ 'n', 'v', 'o' }, '[[', function()
-  local pos = vim.api.nvim_win_get_cursor(0)
-  local buf = vim.api.nvim_get_current_buf()
+  -- BUG: 用 nvim_buf_set_mark() 会有问题
+  vim.fn.win_execute(vim.api.nvim_get_current_win(), "normal! m'")
   require('tree-climber').goto_parent()
-  local after_pos = vim.api.nvim_win_get_cursor(0)
-  if pos[1] ~= after_pos[1] or pos[2] ~= after_pos[2] then
-    vim.api.nvim_buf_set_mark(buf, [[']], pos[1], pos[2], {})
-  end
 end, { noremap = true, silent = true, buffer = true })
