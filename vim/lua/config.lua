80046f8d4aaf9118bb7b96d9dc359aa045dff051
eph <eph@MacBook-Pro.local>
2025-01-15 03:41:29 +0800

    update

--- vim/lua/config.lua
+++ vim/lua/config.lua
@@ -14,11 +14,9 @@ local loop = vim.uv or vim.loop
 --vim.g.base46_cache = config_path .. '/base46_cache/'
 
 -- 简易覆盖 vim.notify, 用来避免 hit enter 信息, 例如 lazy.nvim 启动时的警告
-if not vim.g.enable_noice then
-  vim.notify = function(msg, level, opts) ---@diagnostic disable-line
-    print(string.format('[%d]', level), msg)
-    vim.cmd.redraw() -- 避免 hit enter
-  end
+vim.notify = function(msg, level, opts) ---@diagnostic disable-line
+  print(string.format('[%s]', tostring(level)), msg)
+  vim.cmd.redraw() -- 避免 hit enter
 end
 
 require('config.options')
