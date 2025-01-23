fd2d70fae2bae986d3c0bda2ff562c358e4fb8e8
eph <eph@MacBook-Pro.local>
2025-01-23 22:31:26 +0800

    fix typo

--- vim/lua/plugins/snacks.lua
+++ vim/lua/plugins/snacks.lua
@@ -32,7 +32,7 @@ local opts = {
     preset = {
       -- stylua: ignore
       keys = {
-        { icon = " ", key = "F", desc = "Find Manager", action = ":enew | NvimTreeOpen" },
+        { icon = " ", key = "F", desc = "File Manager", action = ":enew | NvimTreeOpen" },
         { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
         { icon = " ", key = "n", desc = "New File", action = ":ene" },
         --{ icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
