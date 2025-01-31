cf9ce96d0eb40aa72dc99e803ea7f4c9d09f1f48
eph <eph@MacBook-Pro.local>
2025-01-31 17:57:00 +0800

    add render-markdown.nvim plugin

---
+++ vim/lua/plugins/render-markdown.lua
@@ -0,0 +1,14 @@
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  ft = { 'markdown', 'Avante' },
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  opts = {
+    -- Whether Markdown should be rendered by default or not
+    enabled = true,
+    file_types = { 'markdown', 'Avante' },
+    code = {
+      enabled = true,
+      sign = false,
+    },
+  },
+}
