28a16f956dd8515e6118fa6c2becfda9c7c8685e
eph <eph@MacBook-Pro.local>
2025-09-18 19:25:23 +0800

    初步把 blink.cmp 插件单独配置

---
+++ vim/lua/plugins/blink.cmp.lua
@@ -0,0 +1,43 @@
+return {
+  'saghen/blink.cmp',
+  version = '*',
+  dependencies = { 'rafamadriz/friendly-snippets', 'epheien/nvim-cmp' },
+  cmd = 'BlinkEnable',
+  config = function()
+    require('blink.cmp').setup({
+      keymap = {
+        preset = 'none', ---@diagnostic disable-line
+        -- 最基本要求
+        ['<C-e>'] = { 'cancel', 'fallback' },
+        ['<CR>'] = { 'select_and_accept', 'fallback' },
+        ['<C-k>'] = { 'select_prev', 'fallback' },
+        ['<C-j>'] = { 'select_next', 'fallback' },
+        ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
+        ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
+        -- 添头
+        ['<C-y>'] = { 'select_and_accept', 'fallback' },
+        ['<C-p>'] = { 'select_prev', 'fallback' },
+        ['<C-n>'] = { 'select_next', 'fallback' },
+        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+      },
+      cmdline = {
+        enabled = true,
+        completion = { menu = { auto_show = true } },
+        keymap = { preset = 'cmdline' },
+        sources = { 'buffer', 'cmdline' },
+      },
+      completion = {
+        list = {
+          selection = {
+            auto_insert = false,
+          },
+        },
+      },
+    })
+    -- dummy, TODO
+    vim.api.nvim_create_user_command('BlinkEnable', function() end, {})
+    -- 启用 blink.cmp 的话就禁用掉 nvim-cmp 的编辑文本补全
+    vim.cmd('silent! CmpDisable')
+  end,
+}
