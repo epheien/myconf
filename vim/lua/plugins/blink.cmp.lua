f1d04d319f37ce8bd2f6686c16d8e10253235edb
eph <eph@MacBook-Pro.local>
2025-09-21 17:38:50 +0800

    blink.cmp 的 cmdline 补全有诸多问题, 暂时还是不能替代 nvim-cmp

--- vim/lua/plugins/blink.cmp.lua
+++ vim/lua/plugins/blink.cmp.lua
@@ -3,7 +3,7 @@ return {
   version = '*',
   dependencies = { 'rafamadriz/friendly-snippets', 'epheien/nvim-cmp' },
   cmd = 'BlinkEnable',
-  keys = { ':', '/', '?' },
+  --keys = { ':', '/', '?' },
   config = function()
     require('plugins.config.blink-cmp')
     -- 启用 blink.cmp 的话就禁用掉 nvim-cmp 的编辑文本补全
