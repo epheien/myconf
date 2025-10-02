2de7a42270d8e9a15913da2101a2d065f5dbf132
eph <eph@MacBook-Pro.local>
2025-10-02 18:56:59 +0800

    修正 basedpyright 默认过多警告的问题

---
+++ vim/lua/config/lsp/basedpyright.lua
@@ -0,0 +1,9 @@
+return {
+  settings = {
+    basedpyright = {
+      analysis = {
+        typeCheckingMode = 'standard', -- "off", "basic", "standard", "strict", "recommended", "all"
+      },
+    },
+  },
+}
