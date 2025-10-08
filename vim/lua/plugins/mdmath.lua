88e4da0b73984934ec9c296c133953c2d3447f5b
eph <eph@MacBook-Pro.local>
2025-10-08 19:09:56 +0800

    update mdmath plugin config

--- vim/lua/plugins/mdmath.lua
+++ vim/lua/plugins/mdmath.lua
@@ -1,9 +1,10 @@
 return {
   'Thiago4532/mdmath.nvim',
+  ft = { 'markdown' },
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
   },
-  opts = { ... },
+  opts = {},
 
   -- The build is already done by default in lazy.nvim, so you don't need
   -- the next line, but you can use the command `:MdMath build` to rebuild
