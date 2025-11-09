bb42d51be4f422f2fbe0ccfb9e010ee90f02f531
eph <eph@MacBook-Pro.local>
2025-11-09 19:08:44 +0800

    add install_nvim.sh

---
+++ install_nvim.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# 自动安装最新的稳定版 nvim (neovim), 安装到 ~/opt/nvim-$kernel-$arch 目录
+set -e
+
+# 1. 创建必要的目录
+mkdir -p ~/opt ~/bin
+
+# 2. 进入 opt 目录
+cd ~/opt
+
+kernel=linux
+if [ $(uname -s) != "Linux" ]; then
+    kernel=macos
+fi
+arch=$(uname -m)
+
+wget -c https://github.com/neovim/neovim/releases/latest/download/nvim-$kernel-$arch.tar.gz -O nvim-$kernel-$arch.tar.gz
+rm -rf ~/opt/nvim-$kernel-$arch/
+tar -xf nvim-$kernel-$arch.tar.gz
+[ -L ~/bin/nvim ] && ln -sfv ~/opt/nvim-$kernel-$arch/bin/nvim ~/bin/
+rm -f nvim-$kernel-$arch.tar.gz
