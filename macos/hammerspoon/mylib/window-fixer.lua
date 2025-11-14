48488177ed79f8ae811a57c18c39b1a282345112
eph <eph@MacBook-Pro.local>
2025-11-15 00:41:39 +0800

    hammerspoon: 初步完成关闭和最小化按钮的拦截更改功能

---
+++ macos/hammerspoon/mylib/window-fixer.lua
@@ -0,0 +1,77 @@
+local M = {}
+
+-- 向上遍历 UI 元素树，找到窗口元素
+-- NOTE: 不一定能找到, 通常点击标题栏都能找到, 已知点击 kitty 终端内容无法获取到(显存内容?)
+local function getWindowFromElement(element)
+  local current = element
+  while current do
+    local role = current:attributeValue('AXRole')
+
+    -- 找到窗口角色
+    if role == 'AXWindow' then
+      local win = current:asHSWindow()
+      return win
+    end
+
+    -- 继续向上查找父元素
+    current = current:attributeValue('AXParent')
+  end
+end
+
+-- 拦截窗口关闭和最小化按钮, 模拟成 Windows 的切换逻辑
+M.winButtonInterceptor = hs.eventtap.new({ hs.eventtap.event.types.leftMouseDown }, function(event)
+  local pos = hs.mouse.absolutePosition()
+  local element = hs.axuielement.systemElementAtPosition(pos.x, pos.y)
+
+  if not element then
+    return false
+  end
+
+  local role = element:attributeValue('AXRole')
+  if role ~= 'AXButton' then
+    return false
+  end
+  local subrole = element:attributeValue('AXSubrole')
+
+  -- 检测最小化按钮
+  if subrole == 'AXMinimizeButton' or subrole == 'AXCloseButton' then
+    local window = getWindowFromElement(element)
+    if not window then
+      return false
+    end
+
+    if window ~= hs.window.focusedWindow() then
+      return false
+    end
+
+    -- 某些特殊窗口暂时无法处理, 例如 Hammerspoon Console
+    if hs.window.orderedWindows()[1] ~= window then
+      return false
+    end
+
+    --local app = window:application()
+    --local appName = app:name()
+
+    print(string.format('拦截并修改 %s 的最小化操作', window:title()))
+
+    local nextWindow = hs.window.orderedWindows()[2]
+    if not nextWindow then
+      return false
+    end
+
+    nextWindow:focus()
+    if Subrole == 'AXMinimizeButton' then
+      window:minimize()
+    else
+      window:close()
+    end
+
+    return true
+  end
+
+  return false
+end)
+
+M.winButtonInterceptor:start()
+
+return M
