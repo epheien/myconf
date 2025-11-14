1181409adb0c53f97f87951009d1555e000dab2f
eph <eph@MacBook-Pro.local>
2025-11-15 02:57:41 +0800

    hammerspoon: minor fix

--- macos/hammerspoon/mylib/sidebar-fixer.lua
+++ macos/hammerspoon/mylib/sidebar-fixer.lua
@@ -7,7 +7,7 @@ if hs.host.operatingSystemVersion().major ~= 13 then
 end
 
 -- 创建一个监听所有窗口的filter
-M.windowCreatedFilter = hs.window.filter.new()
+M.windowFilter = hs.window.filter.new()
 
 -- 所有关闭全部窗口仍然不退出 Dock 栏中的应用的那些应用都要处理
 local toFixApps = {
@@ -35,7 +35,7 @@ for _, name in ipairs(toFixApps) do
 end
 
 -- 监听窗口创建事件
-M.windowCreatedFilter:subscribe(hs.window.filter.windowCreated, function(window, appName, eventName)
+M.windowFilter:subscribe(hs.window.filter.windowCreated, function(window, appName, eventName)
   --print(string.format('新窗口创建(%s): %s (%s)', eventName, appName, window:title()))
   if toFixAppsDict[appName] then
     window:focus()
