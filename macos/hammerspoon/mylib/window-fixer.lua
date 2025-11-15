56c303bb8e6a65c75fde3c1e8f831f909dc6346c
eph <eph@MacBook-Pro.local>
2025-11-16 02:00:59 +0800

    hammerspoon: 捕获微信主窗口的关闭按钮, 改为隐藏, 因为无法捕获其他事件

--- macos/hammerspoon/mylib/window-fixer.lua
+++ macos/hammerspoon/mylib/window-fixer.lua
@@ -5,6 +5,48 @@ local getOrderedWindows = function()
 end
 local getWindowFromElement = require('mylib.utils').getWindowFromElement
 
+M.windowMinOrClsoeHook = function(window, isMinimized, hotkey)
+  if window ~= hs.window.focusedWindow() then
+    return false
+  end
+
+  -- FIXME: 不知为何, 无法监控到微信窗口的新建和关闭事件, 只能用这个方法 hack
+  if not isMinimized and window:application():name() == '微信' then
+    if #window:application():allWindows() == 1 and window:title() == '微信' then
+      hs.timer.doAfter(0.08, function() window:application():hide() end)
+      return true
+    end
+  end
+
+  -- 暂时不处理关闭事件
+  if not isMinimized then
+    return false
+  end
+
+  local orderedWindows = getOrderedWindows()
+
+  -- 某些特殊窗口暂时无法处理, 例如 Hammerspoon Console
+  if orderedWindows[1] ~= window then
+    return false
+  end
+
+  local nextWindow = orderedWindows[2]
+  if not nextWindow then
+    return false
+  end
+
+  --print(string.format('拦截修改 %s 的最小化, 激活窗口为 %s', window:title(), nextWindow:title()))
+
+  nextWindow:focus()
+  if isMinimized then
+    window:minimize()
+  else
+    window:close()
+  end
+
+  return true
+end
+
 -- 拦截窗口关闭和最小化按钮, 模拟成 Windows 的切换逻辑
 M.winButtonInterceptor = hs.eventtap.new({ hs.eventtap.event.types.leftMouseDown }, function(event)
   local pos = hs.mouse.absolutePosition()
@@ -21,39 +63,12 @@ M.winButtonInterceptor = hs.eventtap.new({ hs.eventtap.event.types.leftMouseDown
   local subrole = element:attributeValue('AXSubrole')
 
   -- 检测最小化按钮
-  --if subrole == 'AXMinimizeButton' or subrole == 'AXCloseButton' then
-  if subrole == 'AXMinimizeButton' then
+  if subrole == 'AXMinimizeButton' or subrole == 'AXCloseButton' then
     local window = getWindowFromElement(element)
     if not window then
       return false
     end
-
-    if window ~= hs.window.focusedWindow() then
-      return false
-    end
-
-    local orderedWindows = getOrderedWindows()
-
-    -- 某些特殊窗口暂时无法处理, 例如 Hammerspoon Console
-    if orderedWindows[1] ~= window then
-      return false
-    end
-
-    local nextWindow = orderedWindows[2]
-    if not nextWindow then
-      return false
-    end
-
-    --print(string.format('拦截修改 %s 的最小化, 激活窗口为 %s', window:title(), nextWindow:title()))
-
-    nextWindow:focus()
-    if subrole == 'AXMinimizeButton' then
-      window:minimize()
-    else
-      window:close()
-    end
-
-    return true
+    return M.windowMinOrClsoeHook(window, subrole == 'AXMinimizeButton', false)
   end
 
   return false
@@ -77,24 +92,7 @@ M.minimizeKeyWatcher = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, func
     if not window then
       return false
     end
-    local orderedWindows = getOrderedWindows()
-    -- 某些特殊窗口暂时无法处理, 例如 Hammerspoon Console
-    if orderedWindows[1] ~= window then
-      return false
-    end
-    local nextWindow = orderedWindows[2]
-    if not nextWindow then
-      return false
-    end
-
-    nextWindow:focus()
-    if keyCode == 46 then
-      window:minimize()
-    else
-      window:close()
-    end
-
-    return true
+    return M.windowMinOrClsoeHook(window, keyCode == 46, true)
   end
 
   return false
