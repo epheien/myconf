17f113aa2a3a0277ed26c383da7b85dadd4f1ded
eph <eph@MacBook-Pro.local>
2025-11-15 23:32:59 +0800

    hammerspoon: add mylib.utils

---
+++ macos/hammerspoon/mylib/utils.lua
@@ -0,0 +1,60 @@
+local M = {}
+
+function M.getOrderedWindows(fallback, excludeApps)
+  excludeApps = excludeApps or {}
+  local code, resp, _ = hs.http.get('http://127.0.0.1:7749/orderedWindows')
+  if code == 200 then
+    --print(resp)
+    local windows = hs.json.decode(resp)
+    local result = {}
+    local allWindows = hs.window.allWindows()
+    for _, win in ipairs(windows) do
+      local found = false
+      for _, hsWin in ipairs(allWindows) do
+        if win.windowNumber == hsWin:id() then
+          found = true
+          -- 排除一些应用的特殊窗口, 例如 Sidebar 的 Settings 窗口无法正常工作
+          -- Sidebar 的 Settings 窗口很奇怪, 聚焦后再调用 window:minimize() 会失效
+          local appName = hsWin:application():name()
+          if excludeApps[appName] then
+            -- ignore
+          else
+            table.insert(result, hsWin)
+            break
+          end
+        end
+      end
+      if not found then
+        hs.alert.show(string.format('window %s not found', win.windowNumber))
+        print(string.format('window not found: %s', hs.json.encode(win)))
+      end
+      -- 仅需要 2 个条目即可
+      if #result == 2 then
+        break
+      end
+    end
+    return result
+  else
+    return fallback and hs.window.orderedWindows() or {}
+  end
+end
+
+-- 向上遍历 UI 元素树，找到窗口元素
+-- NOTE: 不一定能找到, 通常点击标题栏都能找到, 已知点击 kitty 终端内容无法获取到(显存内容?)
+function M.getWindowFromElement(element)
+  local current = element
+  while current do
+    local role = current:attributeValue('AXRole')
+
+    -- 找到窗口角色
+    if role == 'AXWindow' then
+      local win = current:asHSWindow()
+      return win
+    end
+
+    -- 继续向上查找父元素
+    current = current:attributeValue('AXParent')
+  end
+end
+
+return M
--- macos/hammerspoon/mylib/window-fixer.lua
+++ macos/hammerspoon/mylib/window-fixer.lua
@@ -1,63 +1,9 @@
 local M = {}
 
-local function getOrderedWindows()
-  local code, resp, info = hs.http.get('http://127.0.0.1:7749/orderedWindows')
-  if code == 200 then
-    --print(resp)
-    local windows = hs.json.decode(resp)
-    local result = {}
-    local allWindows = hs.window.allWindows()
-    for idx, win in ipairs(windows) do
-      local found = false
-      for _, hsWin in ipairs(allWindows) do
-        if win.windowNumber == hsWin:id() then
-          found = true
-          -- 排除一些应用的特殊窗口, 例如 Sidebar 的 Settings 窗口无法正常工作
-          -- Sidebar 的 Settings 窗口很奇怪, 聚焦后再调用 window:minimize() 会失效
-          local appName = hsWin:application():name()
-          if appName == 'Sidebar' then
-            -- ignore
-          else
-            table.insert(result, hsWin)
-            break
-          end
-        end
-      end
-      if not found then
-        hs.alert.show(string.format('window %s not found', win.windowNumber))
-        print(string.format('window not found: %s', hs.json.encode(win)))
-      end
-      -- 仅需要 2 个条目即可
-      if #result == 2 then
-        break
-      end
-    end
-    --print(string.format('result: %s', #result))
-    return result
-  else
-    print('**NOTE:** window-fixer fallback to hs.window.orderedWindows()')
-    --return hs.window.orderedWindows()
-    return {}
-  end
+local getOrderedWindows = function()
+  return require('mylib.utils').getOrderedWindows(false, { ['Sidebar'] = true })
 end
-
--- 向上遍历 UI 元素树，找到窗口元素
--- NOTE: 不一定能找到, 通常点击标题栏都能找到, 已知点击 kitty 终端内容无法获取到(显存内容?)
-local function getWindowFromElement(element)
-  local current = element
-  while current do
-    local role = current:attributeValue('AXRole')
-
-    -- 找到窗口角色
-    if role == 'AXWindow' then
-      local win = current:asHSWindow()
-      return win
-    end
-
-    -- 继续向上查找父元素
-    current = current:attributeValue('AXParent')
-  end
-end
+local getWindowFromElement = require('mylib.utils').getWindowFromElement
 
 -- 拦截窗口关闭和最小化按钮, 模拟成 Windows 的切换逻辑
 M.winButtonInterceptor = hs.eventtap.new({ hs.eventtap.event.types.leftMouseDown }, function(event)
@@ -116,7 +62,7 @@ end)
 M.winButtonInterceptor:start()
 
 -- 拦截 Cmd+M 和 Cmd+W 快捷键
-minimizeKeyWatcher = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, function(event)
+M.minimizeKeyWatcher = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, function(event)
   local flags = event:getFlags()
   local keyCode = event:getKeyCode()
 
@@ -154,6 +100,6 @@ minimizeKeyWatcher = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, functi
   return false
 end)
 
-minimizeKeyWatcher:start()
+M.minimizeKeyWatcher:start()
 
 return M
