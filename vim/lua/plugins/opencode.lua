6ded3a75a844bfa59fdef89d219bfecdca7029e0
eph <eph@MacBook-Pro.local>
2026-01-09 16:08:23 +0800

    vim: add opencode.nvim

---
+++ vim/lua/plugins/opencode.lua
@@ -0,0 +1,62 @@
+return {
+  'sudo-tee/opencode.nvim',
+  cmd = { 'Opencode' },
+  config = function(_plug, _opts) require('opencode').setup(_opts) end,
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'MeanderingProgrammer/render-markdown.nvim',
+    -- Optional, for file mentions and commands completion, pick only one
+    -- 'saghen/blink.cmp',
+    'hrsh7th/nvim-cmp',
+
+    -- Optional, for file mentions picker, pick only one
+    -- 'folke/snacks.nvim',
+    'nvim-telescope/telescope.nvim',
+    -- 'ibhagwan/fzf-lua',
+    -- 'nvim_mini/mini.nvim',
+  },
+  opts = {
+    preferred_picker = 'telescope',
+    preferred_completion = 'nvim-cmp',
+    keymap = {
+      input_window = {
+        ['<cr>'] = false,
+        ['<C-s>'] = { 'submit_input_prompt', mode = { 'n', 'i' } }, -- Submit prompt (normal mode and insert mode)
+        ['<esc>'] = false,
+        ['<C-k>'] = {
+          function()
+            local ok, val = pcall(function() return require('cmp').core.view:visible() end)
+            if ok and val then
+              vim.cmd([[call feedkeys("\<Up>")]])
+            else
+              vim.cmd([[call feedkeys("\<Esc>\<C-w>k")]])
+            end
+          end,
+          mode = { 'i' },
+        },
+        ['<C-j>'] = {
+          function()
+            local ok, val = pcall(function() return require('cmp').core.view:visible() end)
+            if ok and val then
+              vim.cmd([[call feedkeys("\<Down>")]])
+            else
+              vim.cmd([[call feedkeys("\<Esc>\<C-w>j")]])
+            end
+          end,
+          mode = { 'i' },
+        },
+        ['<up>'] = false,
+        ['<down>'] = false,
+        ['<C-p>'] = { 'prev_prompt_history', mode = { 'i' } }, -- Navigate to previous prompt in history
+        ['<C-n>'] = { 'next_prompt_history', mode = { 'i' } }, -- Navigate to next prompt in history
+      },
+      output_window = {
+        ['<esc>'] = false,
+      },
+    },
+    ui = {
+      position = 'current',
+      input_height = 0.1,
+    },
+  },
+}
--- vim/lua/plugins/render-markdown.lua
+++ vim/lua/plugins/render-markdown.lua
@@ -1,11 +1,11 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
-  ft = { 'markdown', 'Avante', 'codecompanion' },
+  ft = { 'markdown', 'Avante', 'codecompanion', 'opencode_output' },
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
   opts = {
     -- Whether Markdown should be rendered by default or not
     enabled = true,
-    file_types = { 'markdown', 'Avante', 'codecompanion' },
+    file_types = { 'markdown', 'Avante', 'codecompanion', 'opencode_output' },
     code = {
       enabled = true,
       sign = false,
