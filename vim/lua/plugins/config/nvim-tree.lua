e8aa146b9bb364b47a6c6caa5fb5c4b4a253d40c
eph <eph@MacBook-Pro.local>
2026-01-18 15:35:11 +0800

    vim: update nvim-tree config
    现在默认使用 floatwin 的方式打开

--- vim/lua/plugins/config/nvim-tree.lua
+++ vim/lua/plugins/config/nvim-tree.lua
@@ -157,6 +157,29 @@ local nvim_tree_opts = {
   view = {
     width = 31,
     signcolumn = 'auto',
+    float = {
+      enable = true,
+      open_win_config = function()
+        local screen_w = vim.opt.columns:get()
+        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+        local window_w = screen_w * 0.5
+        local window_h = screen_h * 0.8
+        local window_w_int = math.floor(window_w)
+        local window_h_int = math.floor(window_h)
+        local center_x = (screen_w - window_w) / 2
+        local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+        return {
+          border = 'single',
+          relative = 'editor',
+          row = center_y,
+          col = center_x,
+          width = window_w_int,
+          height = window_h_int,
+          title = ' NvimTree ',
+          title_pos = 'center',
+        }
+      end,
+    },
   },
   renderer = {
     group_empty = true,
