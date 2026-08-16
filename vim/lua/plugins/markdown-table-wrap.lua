c6982ad5e5688174a6153e6a420b5b74961d3459
eph <eph@MacBook-Pro.local>
2026-08-16 17:36:29 +0800

    feat(vim): 新增 markdown-table-wrap 插件, markdown 表格过长时自动换行

---
+++ vim/lua/plugins/markdown-table-wrap.lua
@@ -0,0 +1,8 @@
+return {
+  'ice345/markdown-table-wrap.nvim',
+  ft = { 'markdown', 'Avante', 'codecompanion', 'opencode_output' },
+  opts = {
+    preview_mode = 'inline',
+    inline_wrap_scope = 'always',
+  },
+}
